bc86c9e537518c13da4248478c3409313fd33471
ChhoengRamekh <RamekhChhoeng023@gmail.com>
2019-08-27 20:58:20 +0700

    Initialize ios smart laundry application

---
+++ LaundryBookingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LaundryBookingApp
+//
+//  Created by ChhoengRamekh on 8/27/19.
+//  Copyright © 2019 ChhoengRamekh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
